03cd0f230f95432c7e8c7d4eb0c2c17609c5e627
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-20 02:46:14 +0300

    Add morgan logger and debug

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import logger from 'morgan';
+import debug from 'debug';
+
+const appDebugger = debug('pokedex:app');
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.send("We're up and running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => appDebugger(`Listening on port ${PORT}`));
