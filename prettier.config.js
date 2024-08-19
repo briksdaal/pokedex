97d6a3ad27dd8925db70124f55ccfb10489fb015
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-20 02:28:20 +0300

    Configure eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: true
+};
+
+export default config;
