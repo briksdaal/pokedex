d1b45d973c9c1c257ef00f6dd2227b81e1142cac
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-21 05:59:02 +0300

    Add all gen 1 types

--- db/populate/types.js
+++ db/populate/types.js
@@ -1,12 +1,19 @@
 const types = [
   { type: 'Normal' },
+  { type: 'Fire', color: 'f42' },
+  { type: 'Water', color: '39f' },
+  { type: 'Electric', color: 'fc3' },
+  { type: 'Grass', color: '7c5' },
+  { type: 'Ice', color: '6cf' },
   { type: 'Fighting', color: 'b54' },
+  { type: 'Poison', color: 'a59' },
+  { type: 'Ground', color: 'db5' },
   { type: 'Flying', color: '8798fe' },
   { type: 'Psychic', color: 'f59' },
-  { type: 'Water', color: '39f' },
-  { type: 'Grass', color: '7c5' },
-  { type: 'Fire', color: 'f42' },
-  { type: 'Poison', color: 'a59' }
+  { type: 'Bug', color: 'ab2' },
+  { type: 'Rock', color: 'ba6' },
+  { type: 'Ghost', color: '66b' },
+  { type: 'Dragon', color: '76e' }
 ];
 
 export default types;
