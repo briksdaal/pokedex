d282d4b0d42deeb7f8091549cfa926e10790e909
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-26 03:56:14 +0300

    Add image download script to use before populate

---
+++ db/populate/downloadImages.js
@@ -0,0 +1,45 @@
+import pokemon from './pokemon.js';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import debug from 'debug';
+import path from 'path';
+import { Readable } from 'stream';
+import { finished } from 'stream/promises';
+
+const downloadDebugger = debug('pokedex:download');
+const destinationDir = './db/populate/downloaded_images/';
+
+async function createDirectory() {
+  if (!fs.existsSync(destinationDir)) {
+    return fsPromises.mkdir(destinationDir);
+  }
+}
+
+async function downloadAndWriteFile(url) {
+  const filename = url.split('/').pop();
+  const destination = path.resolve(destinationDir, filename);
+  const imageRes = await fetch(url);
+  const writeStream = fs.createWriteStream(destination);
+  return finished(Readable.fromWeb(imageRes.body).pipe(writeStream));
+}
+
+async function main() {
+  try {
+    downloadDebugger('Checking if directory exists, creating if needed...');
+    await createDirectory();
+    downloadDebugger('Starting image downloads...');
+    for (let p of pokemon) {
+      if (!p.image || !p.image.includes('http')) {
+        downloadDebugger(`Skipping ${p.name}, doesn't seem like a url...`);
+        continue;
+      }
+      downloadDebugger(`Downloading ${p.name} image...`);
+      await downloadAndWriteFile(p.image);
+    }
+    downloadDebugger('Finished all downloads');
+  } catch (e) {
+    downloadDebugger(e);
+  }
+}
+
+main();
